ec2001d4c07c4a5da120ae209b5cc812ccf675b4
rian lucas <rianluccas08@gmail.com>
2023-05-04 20:12:06 -0300

    feat: implement default theme and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-export default function App() {
+import { Button } from './components/button'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
+export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
