5934bba795556aeb455f0ec09142d6a4173de151
rian lucas <rianluccas08@gmail.com>
2023-05-10 18:01:37 -0300

    refactor: remove button component

--- src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './button.styles.js'
-
-interface ButtonProps {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
