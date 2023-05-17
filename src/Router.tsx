3a062596262b97f28655aef84b17047a70eff315
rian lucas <rianluccas08@gmail.com>
2023-05-17 19:34:37 -0300

    fix: corrects import of router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/History'
-import { History } from './pages/Home'
+import { History } from './pages/History'
+import { Home } from './pages/Home/index'
 import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
