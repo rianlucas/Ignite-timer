a833de6dcbf4817900d9c23f20826fe594375056
rian lucas <rianluccas08@gmail.com>
2023-05-17 18:18:37 -0300

    feat: create a main route with the '/' path

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/History'
-import History from './pages/Home'
+import { Home } from './pages/History'
+import { History } from './pages/Home'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
-export default function Router() {
+export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
