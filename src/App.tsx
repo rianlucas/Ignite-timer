83d41600e51dce0bde66c66662bfd1bc39375aea
rian lucas <rianluccas08@gmail.com>
2023-05-17 18:20:11 -0300

    fix: correct Router import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { BrowserRouter } from 'react-router-dom'
-import Router from './Router'
+import { Router } from './Router'
 
 export default function App() {
   return (
