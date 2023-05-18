fa2cefc247a10aadb9266c21d2f8addaee43e625
Rian Lucas <rianluccas08@gmail.com>
2023-05-18 19:41:55 -0300

    feat: History page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { History } from './pages/History'
+import { History } from './pages/History/index'
 import { Home } from './pages/Home/index'
 import { DefaultLayout } from './layouts/DefaultLayout'
 
